c91eebc4f883648ee88f2f0e98fe1115362884de
aquinzi <arielaquinzi@gmail.com>
2018-10-29 10:57:17 -0300

    cambios estilo en la forma para mostrar videos de conferencia educación arreglo breadcrumbs cuando el título es largo

--- themes/mrg/js/sitio.js
+++ themes/mrg/js/sitio.js
@@ -17,51 +17,70 @@ jQuery(document).ready(function() {
 		}
 	});
 	
-	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur .profesionales-videos video").css("display","none");
+	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur .videos-thumbnails-list video").css("display","none");
+	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur-conferencias-y-ponencias .videos-thumbnails-list iframe").css("display","none");
 	
 	
 	
 	
 	// Page: ciudad
 	
-	 jQuery('.slider.simple').flexslider({
-		 animation: "slide",
-			selector: ".slides > div", 
-			directionNav: false,
-			smoothHeight: true,
-	  });
+	jQuery('.slider.simple').flexslider({
+		animation: "slide",
+		selector: ".slides > div", 
+		directionNav: false,
+		smoothHeight: true,
+	});
 	  
 	  
-	  var slides_special = ["economia", "aves", "monumentos", "riqueza", "malvinas"];
+	var slides_special = ["economia", "aves", "monumentos", "riqueza", "malvinas"];
 	
 		
-		for(var i = 0; i < slides_special.length; i++) {
-		 
-		 
-			jQuery("#slide-" + slides_special[i] + "-thumbnails").html(jQuery("#slide-" + slides_special[i] + " .slides").clone());
-			jQuery("#slide-" + slides_special[i] + "-thumbnails").flexslider({
-				animation: "slide",
-				selector: ".slides > div", 
-				controlNav: false,
-				animationLoop: false,
-				slideshow: false,
-				itemWidth: 150,
-				itemMargin: 2,
-				directionNav: true,
-				asNavFor: "#slide-" + slides_special[i],
-			});
+	for(var i = 0; i < slides_special.length; i++) {
+		jQuery("#slide-" + slides_special[i] + "-thumbnails").html(jQuery("#slide-" + slides_special[i] + " .slides").clone());
+		jQuery("#slide-" + slides_special[i] + "-thumbnails").flexslider({
+			animation: "slide",
+			selector: ".slides > div", 
+			controlNav: false,
+			animationLoop: false,
+			slideshow: false,
+			itemWidth: 150,
+			itemMargin: 2,
+			directionNav: true,
+			asNavFor: "#slide-" + slides_special[i],
+		});
 
-			jQuery("#slide-" + slides_special[i] ).flexslider({
-				selector: ".slides > div", 
-				animation: "slide",
-				controlNav: false,
-				animationLoop: false,
-				slideshow: false,
-				smoothHeight: true,
-				sync: "#slide-" + slides_special[i] + "-thumbnails",
-			});	 
-		 
+		jQuery("#slide-" + slides_special[i] ).flexslider({
+			selector: ".slides > div", 
+			animation: "slide",
+			controlNav: false,
+			animationLoop: false,
+			slideshow: false,
+			smoothHeight: true,
+			sync: "#slide-" + slides_special[i] + "-thumbnails",
+		});	 
+	 
+	}
+	
+	
+	// page: Espacio Tecnológico 
+	jQuery("body.page-espacio-tecnologico fieldset p").hide();
+	jQuery("body.page-espacio-tecnologico fieldset .mauticform-checkboxgrp-row").hide();
+	jQuery("body.page-espacio-tecnologico legend").css("cursor","pointer");
+	jQuery("body.page-espacio-tecnologico legend").append('<span class="arrow arrow-down"></span>');
+	jQuery("body.page-espacio-tecnologico legend").on("click",function(){
+		
+		jQuery(this).next().slideToggle();
+
+		if ( jQuery(this).children("span").hasClass("arrow-down") ) {
+			  jQuery(this).children("span").addClass("arrow-left");
+			  jQuery(this).children("span").removeClass("arrow-down");
 		}
+		else {
+			jQuery(this).children("span").addClass("arrow-down");
+			jQuery(this).children("span").removeClass("arrow-left");
+		}
+	});
 	
 	
 });
