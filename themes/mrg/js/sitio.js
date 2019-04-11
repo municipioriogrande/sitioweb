9c61f2d88c2d0714e573c8d5580983b85b305f9b
aquinzi <arielaquinzi@gmail.com>
2019-04-11 11:10:18 -0300

    arreglo :bug: de acordion en congreso educacion

--- themes/mrg/js/sitio.js
+++ themes/mrg/js/sitio.js
@@ -1,28 +1,26 @@
 jQuery(document).ready(function() {
 	// page: congreso 
-	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur .accordion .mesas-container").hide();
-	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur .accordion .time").css("cursor","pointer");
-	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur .accordion .time").append('<span class="arrow arrow-down"></span>');
-	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur .accordion .time").on("click",function(){
+
+	jQuery(".congreso-educacion-accordion .mesas-container").hide();
+	jQuery(".congreso-educacion-accordion .time").css("cursor","pointer");
+	jQuery(".congreso-educacion-accordion .time").append('<span class="arrow arrow-down"></span>');
+	jQuery(".congreso-educacion-accordion .time").on("click",function(){
 		
 		jQuery(this).next().slideToggle();
 			//alert(jQuery(this).children("arrow"));
-		if ( jQuery(this).children("span").hasClass("arrow-down") ) {
-			jQuery(this).children("span").addClass("arrow-left");
-			jQuery(this).children("span").removeClass("arrow-down");
-		}
-		else {
-			jQuery(this).children("span").addClass("arrow-down");
-			jQuery(this).children("span").removeClass("arrow-left");
-		}
+			if ( jQuery(this).children("span").hasClass("arrow-down") ) {
+				  jQuery(this).children("span").addClass("arrow-left");
+				  jQuery(this).children("span").removeClass("arrow-down");
+			}
+			else {
+				  jQuery(this).children("span").addClass("arrow-down");
+				  jQuery(this).children("span").removeClass("arrow-left");
+			}
 	});
 	
-	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur .videos-thumbnails-list video").css("display","none");
-	jQuery("body.page-1er-congreso-internacional-educacion-e-inclusion-desde-el-sur-conferencias-y-ponencias .videos-thumbnails-list iframe").css("display","none");
-	
-	
-	
-	
+	jQuery(".videos-thumbnails-list video").css("display","none");
+	jQuery(".videos-thumbnails-list iframe").css("display","none");
+
 	// Page: ciudad
 	
 	jQuery('.slider.simple').flexslider({
@@ -32,10 +30,8 @@ jQuery(document).ready(function() {
 		smoothHeight: true,
 	});
 	  
-	  
 	var slides_special = ["economia", "aves", "monumentos", "riqueza", "malvinas"];
 	
-		
 	for(var i = 0; i < slides_special.length; i++) {
 		jQuery("#slide-" + slides_special[i] + "-thumbnails").html(jQuery("#slide-" + slides_special[i] + " .slides").clone());
 		jQuery("#slide-" + slides_special[i] + "-thumbnails").flexslider({
@@ -62,7 +58,6 @@ jQuery(document).ready(function() {
 	 
 	}
 	
-	
 	// page: Espacio Tecnológico 
 	jQuery("body.page-espacio-tecnologico fieldset p").hide();
 	jQuery("body.page-espacio-tecnologico fieldset .mauticform-checkboxgrp-row").hide();
@@ -81,6 +76,5 @@ jQuery(document).ready(function() {
 			jQuery(this).children("span").removeClass("arrow-left");
 		}
 	});
-	
 	
 });
