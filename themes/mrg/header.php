458451e63327205b2227ee8e94e360d9dd980950
aquinzi <arielaquinzi@gmail.com>
2020-01-16 09:43:02 -0300

    arreglos estilo

--- themes/mrg/header.php
+++ themes/mrg/header.php
@@ -480,253 +480,283 @@ if ( get_the_excerpt($post) ) {
 			<!--<div id="mobile_nav_icon"></div>-->
 		
 			<div id="menu_wrapper">
-				
-                <?php
-					// THISNEW Display Hamburguesa menu
-					$pp_hamburguesa_menu = get_option('pp_hamburguesa_menu');
-					if(!empty($pp_hamburguesa_menu))
-					{
-				?>
-                <div class="hamburger hamburger--squeeze">
-                    <div class="hamburger-box">
-                        <div class="hamburger-inner"></div>
-                    </div>
-                </div>
-				<?php 
-					}
-				?>
-                
-				<!-- Begin logo -->	
-				<?php
-					$use_transparent_logo = FALSE;
-					if(isset($pp_homepage_style) OR !empty($page_menu_transparent))
-				    {
-					    if($pp_homepage_style=='fullscreen' OR $pp_homepage_style=='fullscreen_video' OR $pp_homepage_style=='flip' OR !empty($pp_page_bg) OR !empty($page_menu_transparent))
-					    {
-					    	$use_transparent_logo = TRUE;
-					    }
-				    }
-				
-				    //get custom logo
-				    $pp_logo = get_option('pp_logo');
-				    $pp_retina_logo = get_option('pp_retina_logo');
-				    $pp_retina_logo_width = 0;
-				    $pp_retina_logo_height = 0;
-				    			
-				    if(empty($pp_logo) && empty($pp_retina_logo))
-				    {	
-				    	$pp_retina_logo = get_template_directory_uri().'/images/logo@2x.png';
-				    	$pp_retina_logo_width = 165;
-				    	$pp_retina_logo_height = 62;
-				    }
 
-				    if(!empty($pp_retina_logo))
-				    {	
-				    	if(empty($pp_retina_logo_width) && empty($pp_retina_logo_height))
-				    	{
-				    		//Get image width and height
-				    		$pp_retina_logo_id = pp_get_image_id($pp_retina_logo);
-				    		$image_logo = wp_get_attachment_image_src($pp_retina_logo_id, 'original');
-				    		
-				    		$pp_retina_logo = $image_logo[0];
-				    		$pp_retina_logo_width = $image_logo[1]/2;
-				    		$pp_retina_logo_height = $image_logo[2]/2;
-				    	}
-				?>		
-				    <a id="custom_logo" class="logo_wrapper <?php if($use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
-				    	<img src="<?php echo $pp_retina_logo; ?>" alt="<?php echo SITE_LOGO_ALT;?>" width="<?php echo $pp_retina_logo_width; ?>" height="<?php echo $pp_retina_logo_height; ?>"/>
-				    </a>
-				<?php
-				    }
-				    else //if not retina logo
-				    {
-				?>
-				    <a id="custom_logo" class="logo_wrapper <?php if($use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
-				    	<img src="<?php echo $pp_logo?>" alt="<?php echo SITE_LOGO_ALT;?>"/>
-				    </a>
-				<?php
-				    }
-				?>
-				
-				<?php
-					//get custom logo transparent
-				    $pp_logo_transparent = get_option('pp_logo_transparent');
-				    $pp_retina_logo_transparent = get_option('pp_retina_logo_transparent');
-				    $pp_retina_logo_transparent_width = 0;
-				    $pp_retina_logo_transparent_height = 0;
+			 	<div class="col col-left">
+
+									<?php
+										// THISNEW Display Hamburguesa menu
+										$pp_hamburguesa_menu = get_option('pp_hamburguesa_menu');
+										if(!empty($pp_hamburguesa_menu))
+										{
+									?>
+										<div class="hamburger hamburger--squeeze">
+											<div class="hamburger-box">
+													<div class="hamburger-inner"></div>
+											</div>
+										</div>
+									<?php 
+										}
+									?>
+										
+									<!-- Begin logo -->	
+									<?php
+										$use_transparent_logo = FALSE;
+										if(isset($pp_homepage_style) OR !empty($page_menu_transparent))
+										{
+											if($pp_homepage_style=='fullscreen' OR $pp_homepage_style=='fullscreen_video' OR $pp_homepage_style=='flip' OR !empty($pp_page_bg) OR !empty($page_menu_transparent))
+											{
+												$use_transparent_logo = TRUE;
+											}
+										}
+									
+										//get custom logo
+										$pp_logo = get_option('pp_logo');
+										$pp_retina_logo = get_option('pp_retina_logo');
+										$pp_retina_logo_width = 0;
+										$pp_retina_logo_height = 0;
+													
+										if(empty($pp_logo) && empty($pp_retina_logo))
+										{	
+											$pp_retina_logo = get_template_directory_uri().'/images/logo@2x.png';
+											$pp_retina_logo_width = 165;
+											$pp_retina_logo_height = 62;
+										}
+
+										if(!empty($pp_retina_logo))
+										{	
+											if(empty($pp_retina_logo_width) && empty($pp_retina_logo_height))
+											{
+												//Get image width and height
+												$pp_retina_logo_id = pp_get_image_id($pp_retina_logo);
+												$image_logo = wp_get_attachment_image_src($pp_retina_logo_id, 'original');
+												
+												$pp_retina_logo = $image_logo[0];
+												$pp_retina_logo_width = $image_logo[1]/2;
+												$pp_retina_logo_height = $image_logo[2]/2;
+											}
+									?>		
+										<a id="custom_logo" class="logo_wrapper <?php if($use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
+											<img src="<?php echo $pp_retina_logo; ?>" alt="<?php echo SITE_LOGO_ALT;?>" width="<?php echo $pp_retina_logo_width; ?>" height="<?php echo $pp_retina_logo_height; ?>"/>
+										</a>
+									<?php
+										}
+										else //if not retina logo
+										{
+									?>
+										<a id="custom_logo" class="logo_wrapper <?php if($use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
+											<img src="<?php echo $pp_logo?>" alt="<?php echo SITE_LOGO_ALT;?>"/>
+										</a>
+									<?php
+										}
+									?>
+									
+									<?php
+										//get custom logo transparent
+										$pp_logo_transparent = get_option('pp_logo_transparent');
+										$pp_retina_logo_transparent = get_option('pp_retina_logo_transparent');
+										$pp_retina_logo_transparent_width = 0;
+										$pp_retina_logo_transparent_height = 0;
+										
+										if(empty($pp_logo_transparent) && empty($pp_retina_logo_transparent))
+										{
+											$pp_retina_logo_transparent = get_template_directory_uri().'/images/logo@2x.png';
+											$pp_retina_logo_transparent_width = 69;
+											$pp_retina_logo_transparent_height = 33;
+										}
+
+										if(!empty($pp_retina_logo_transparent))
+										{
+											if(empty($pp_retina_logo_transparent_width) && empty($pp_retina_logo_transparent_width))
+											{
+												//Get image width and height
+												$pp_retina_logo_transparent_id = pp_get_image_id($pp_retina_logo_transparent);
+												$image_logo = wp_get_attachment_image_src($pp_retina_logo_transparent_id, 'original');
+												
+												$pp_retina_logo_transparent = $image_logo[0];
+												$pp_retina_logo_transparent_width = $image_logo[1]/2;
+												$pp_retina_logo_transparent_height = $image_logo[2]/2;
+											}
+									?>		
+										<a id="custom_logo_transparent" class="logo_wrapper <?php if(!$use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
+											<img src="<?php echo $pp_retina_logo_transparent; ?>" alt="<?php echo SITE_LOGO_ALT;?>" width="<?php echo $pp_retina_logo_transparent_width; ?>" height="<?php echo $pp_retina_logo_transparent_height; ?>"/>
+										</a>
+									<?php
+										}
+										else //if not retina logo
+										{
+									?>
+										<a id="custom_logo_transparent" class="logo_wrapper <?php if(!$use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
+											<img src="<?php echo $pp_logo_transparent?>" alt="<?php echo SITE_LOGO_ALT;?>"/>
+										</a>
+									<?php
+										}
+									?>
+									<!-- End logo -->
+										
+										<div class="header_line" style="height:<?php echo $pp_retina_logo_height; ?>px"></div>
+										
+										<!-- Mayor name -->
+										<?php
+										// THISNEW Display Hamburguesa menu
+										$pp_mayor_show = get_option('pp_mayor_show');
+										if(!empty($pp_mayor_show))
+										{
+											$pp_mayor_name = get_option('pp_mayor_name');
+											$pp_mayor_url = get_option('pp_mayor_url');
+									?>
+										<div class="mayor-name">
+											<div class="mayor-name-content">
+													<?php 
+											if($pp_mayor_url){
+											?>
+													<a href="<?php echo $pp_mayor_url; ?>"><?php echo $pp_mayor_name; ?></a>
+													<?php
+											}else{
+												echo $pp_mayor_name; 
+											}
+											?>
+											</div>
+										</div>
+									<?php 
+										}
+									?>
+										
+										
+
+
+
+				</div>
+
+				<div class="col col-right">
+
 					
-					if(empty($pp_logo_transparent) && empty($pp_retina_logo_transparent))
-				    {
-				    	$pp_retina_logo_transparent = get_template_directory_uri().'/images/logo@2x.png';
-				    	$pp_retina_logo_transparent_width = 69;
-				    	$pp_retina_logo_transparent_height = 33;
-				    }
 
-				    if(!empty($pp_retina_logo_transparent))
-				    {
-				    	if(empty($pp_retina_logo_transparent_width) && empty($pp_retina_logo_transparent_width))
-				    	{
-				    		//Get image width and height
-				    		$pp_retina_logo_transparent_id = pp_get_image_id($pp_retina_logo_transparent);
-				    		$image_logo = wp_get_attachment_image_src($pp_retina_logo_transparent_id, 'original');
-				    		
-				    		$pp_retina_logo_transparent = $image_logo[0];
-				    		$pp_retina_logo_transparent_width = $image_logo[1]/2;
-				    		$pp_retina_logo_transparent_height = $image_logo[2]/2;
-				    	}
-				?>		
-				    <a id="custom_logo_transparent" class="logo_wrapper <?php if(!$use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
-				    	<img src="<?php echo $pp_retina_logo_transparent; ?>" alt="<?php echo SITE_LOGO_ALT;?>" width="<?php echo $pp_retina_logo_transparent_width; ?>" height="<?php echo $pp_retina_logo_transparent_height; ?>"/>
-				    </a>
-				<?php
-				    }
-				    else //if not retina logo
-				    {
-				?>
-				    <a id="custom_logo_transparent" class="logo_wrapper <?php if(!$use_transparent_logo) { ?>hidden<?php } else { ?>default<?php } ?>" href="<?php echo home_url(); ?>">
-				    	<img src="<?php echo $pp_logo_transparent?>" alt="<?php echo SITE_LOGO_ALT;?>"/>
-				    </a>
-				<?php
-				    }
-				?>
-				<!-- End logo -->
-                
-                <div class="header_line" style="height:<?php echo $pp_retina_logo_height; ?>px"></div>
-                
-                <!-- Mayor name -->
-                <?php
-					// THISNEW Display Hamburguesa menu
-					$pp_mayor_show = get_option('pp_mayor_show');
-					if(!empty($pp_mayor_show))
-					{
-						$pp_mayor_name = get_option('pp_mayor_name');
-						$pp_mayor_url = get_option('pp_mayor_url');
-				?>
-                <div class="mayor-name">
-                    <div class="mayor-name-content">
-                        <?php 
-						if($pp_mayor_url){
-						?>
-                        <a href="<?php echo $pp_mayor_url; ?>"><?php echo $pp_mayor_name; ?></a>
-                        <?php
-						}else{
-							echo $pp_mayor_name; 
-						}
-						?>
-                    </div>
-                </div>
-				<?php 
-					}
-				?>
-                
-                
-                
-                <!-- Weather -->
-                <?php
-					// THISNEW Display tehe wather 
-					$pp_wather_show = get_option('pp_wather_show');
-					if(!empty($pp_wather_show))
-					{
-				?>
-                <div class="content-weather">
-                    <div class="content-weather-content">
-                        <?php echo do_shortcode('[do_widget id=andimol_clima-2 wrap=div]'); ?>
-                    </div>
-                </div>
-				<?php 
-					}
-				?>
-				<?php
-					//Display phone button
-					$pp_phone_button_header = get_option('pp_phone_button_header');
-					if(!empty($pp_phone_button_header))
-					{
-						$pp_phone_button_number = get_option('pp_phone_button_number');
-				?>
-				<a href="tel:<?php echo $pp_phone_button_number; ?>">
-					<div class="header_action">
-						<i class="fa fa-phone"></i><?php echo $pp_phone_button_number; ?>
+										
+										<!-- Weather -->
+										<?php
+										// THISNEW Display tehe wather 
+										$pp_wather_show = get_option('pp_wather_show');
+										if(!empty($pp_wather_show))
+										{
+									?>
+										<div class="content-weather">
+											<div class="content-weather-content">
+													<?php echo do_shortcode('[do_widget id=andimol_clima-2 wrap=div]'); ?>
+											</div>
+										</div>
+									<?php 
+										}
+									?>
+									<?php
+										//Display phone button
+										$pp_phone_button_header = get_option('pp_phone_button_header');
+										if(!empty($pp_phone_button_header))
+										{
+											$pp_phone_button_number = get_option('pp_phone_button_number');
+									?>
+									<a href="tel:<?php echo $pp_phone_button_number; ?>">
+										<div class="header_action">
+											<i class="fa fa-phone"></i><?php echo $pp_phone_button_number; ?>
+										</div>
+									</a>
+									<?php 
+										}
+									?>
+										
+										<?php
+										//Display CTA button
+										$pp_cta_button_in_header = get_option('pp_cta_button_in_header');
+										if(!empty($pp_cta_button_in_header))
+										{
+											$pp_cta_button_text = get_option('pp_cta_button_text');
+											$pp_cta_button_url = get_option('pp_cta_button_url');
+									?>
+									<a href="<?php echo $pp_cta_button_url; ?>" class="cta_wrapper">
+										<div class="header_cta">
+											<?php echo $pp_cta_button_text; ?>
+										</div>
+									</a>
+									<?php 
+										}
+									?>
+										
+									
+									<?php
+										//Check if display search in header
+										$pp_ajax_search_header = get_option('pp_ajax_search_header');
+										
+										if(!empty($pp_ajax_search_header))
+										{
+									?>
+									<form role="search" method="get" name="searchform" id="searchform" action="<?php echo home_url(); ?>/">
+										<div>
+											<label for="s"><?php echo _e( 'To Search, type and hit enter', THEMEDOMAIN ); ?></label>
+											<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" autocomplete="off"/>
+											<button>
+												<i class="fa fa-search"></i><span class="visually-hide">Buscar</span>
+											</button>
+										</div>
+										<div id="autocomplete"></div>
+									</form>
+									<?php
+										}
+									?>
+									
+									<?php
+									if(empty($pp_hamburguesa_menu))
+									{
+									?>
+									<!-- Begin main nav -->
+									<div id="nav_wrapper">
+										<div class="nav_wrapper_inner">
+											<div id="menu_border_wrapper">
+												<?php 	
+													if ( has_nav_menu( 'primary-menu' ) ) 
+													{
+														//Get page nav
+														wp_nav_menu( 
+																array( 
+																	'menu_id'			=> 'main_menu',
+																	'menu_class'		=> 'nav',
+																	'theme_location' 	=> 'primary-menu',
+																	'link_before' 		=> '<span>', 
+																	'link_after' 		=> '</span>',
+																	'walker' => new tg_walker(),
+																) 
+														); 
+													}
+													else
+													{
+															echo '<div class="notice">Please setup "Main Menu" using Wordpress Dashboard > Appearance > Menus</div>';
+													}
+												?>
+											</div>
+										</div>
+									</div>
+									<!-- End main nav -->
+									<?php
+									}
+									?>
+									<div class="header-escudo">
+													<img src="<?php echo get_template_directory_uri();?>/images/escudo.png">
+												</div>
+									</div>
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
 					</div>
-				</a>
-				<?php 
-					}
-				?>
-                
-                <?php
-					//Display CTA button
-					$pp_cta_button_in_header = get_option('pp_cta_button_in_header');
-					if(!empty($pp_cta_button_in_header))
-					{
-						$pp_cta_button_text = get_option('pp_cta_button_text');
-						$pp_cta_button_url = get_option('pp_cta_button_url');
-				?>
-				<a href="<?php echo $pp_cta_button_url; ?>" class="cta_wrapper">
-					<div class="header_cta">
-						<?php echo $pp_cta_button_text; ?>
-					</div>
-				</a>
-				<?php 
-					}
-				?>
-                
+
+
 				
-				<?php
-				    //Check if display search in header
-				    $pp_ajax_search_header = get_option('pp_ajax_search_header');
-				    
-				    if(!empty($pp_ajax_search_header))
-				    {
-				?>
-				<form role="search" method="get" name="searchform" id="searchform" action="<?php echo home_url(); ?>/">
-				    <div>
-				    	<label for="s"><?php echo _e( 'To Search, type and hit enter', THEMEDOMAIN ); ?></label>
-				    	<input type="text" value="<?php the_search_query(); ?>" name="s" id="s" autocomplete="off"/>
-				    	<button>
-				        	<i class="fa fa-search"></i><span class="visually-hide">Buscar</span>
-				        </button>
-				    </div>
-				    <div id="autocomplete"></div>
-				</form>
-				<?php
-				    }
-				?>
-				
-				<?php
-				if(empty($pp_hamburguesa_menu))
-				{
-				?>
-			    <!-- Begin main nav -->
-			    <div id="nav_wrapper">
-			    	<div class="nav_wrapper_inner">
-			    		<div id="menu_border_wrapper">
-			    			<?php 	
-			    				if ( has_nav_menu( 'primary-menu' ) ) 
-			    				{
-				    			    //Get page nav
-				    			    wp_nav_menu( 
-				    			        	array( 
-				    			        		'menu_id'			=> 'main_menu',
-				    			        		'menu_class'		=> 'nav',
-				    			        		'theme_location' 	=> 'primary-menu',
-				    			        		'link_before' 		=> '<span>', 
-				    			        		'link_after' 		=> '</span>',
-				    			        		'walker' => new tg_walker(),
-				    			        	) 
-				    			    ); 
-				    			}
-				    			else
-							    {
-							     		echo '<div class="notice">Please setup "Main Menu" using Wordpress Dashboard > Appearance > Menus</div>';
-							    }
-			    			?>
-			    		</div>
-			    	</div>
-			    </div>
-			    <!-- End main nav -->
-				<?php
-				}
-				?>
-			    </div>
+				 
 			</div>
             
             
@@ -766,8 +796,8 @@ if ( get_the_excerpt($post) ) {
 			}
 			?>
             
+						
 		</div>
-		
 		<?php
 			} //End if not blank template
 		?>
